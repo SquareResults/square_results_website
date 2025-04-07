ca0fce1d0f3a973bfca517c8ba0c3b11f4435370
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2025-04-07 14:47:46 -0400

    updates navbar for dropdown menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,30 @@ import Link from "next/link"; // ✅ Use Next.js Link
 import { useRouter } from "next/router"; // ✅ Use Next.js Router
 import { Menu, X } from "lucide-react";
 import { DemoForm } from "./DemoForm";
+import { FaAngleDown, FaUser, FaUsers } from "react-icons/fa";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [showDemoForm, setShowDemoForm] = useState(false);
   const router = useRouter(); // ✅ Next.js router
+
+  const menuItems = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    {
+      name: "Services",
+      path: "/services",
+      submenu: [
+        { name: "Job Seekers", path: "/services/job-seekers" },
+        { name: "Hiring Partners", path: "/services/hiring-partners" },
+      ],
+    },
+    { name: "Resources", path: "/resources" },
+    { name: "Contact", path: "/contact" },
+    { name: "Careers", path: "/careers" },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -40,27 +58,53 @@ const Navbar = () => {
           <img src="/images/SQRlogo.jpg" alt="Logo" className="w-15 h-14" />
         </Link>
         <div className="hidden md:flex items-center justify-end flex-wrap text-xl space-x-14 ml-auto">
-          {[
-            "",
-            "home",
-            "about",
-            "services",
-            "resources",
-            "contact",
-            "careers",
-          ].map((item) => (
+          {menuItems.map((item) => (
             <motion.div
-              key={item}
+              key={item.name}
               className={`text-semantic-white transition-colors duration-200 ${
-                isActive(item === "home" ? "/" : `/${item}`)
+                isActive(item.path)
                   ? "text-primary-light font-bold"
                   : "hover:text-primary-light"
               }`}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}>
-              <Link href={item === "home" ? "/" : `/${item}`}>
-                {item.charAt(0).toUpperCase() + item.slice(1)}
-              </Link>
+              {item.submenu ? (
+                <Popover>
+                  <PopoverTrigger className="inline-flex items-center gap-x-1">
+                    <span>{item.name}</span>
+                    {/* <FaAngleDown className="text-primary-light" /> */}
+                  </PopoverTrigger>
+
+                  <PopoverContent className="w-screen max-w-sm flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 ring-1 shadow-lg ring-gray-900/5">
+                    <div className="flex flex-col p-4 bg-semantic-white">
+                      {item.submenu.map((subItem) => (
+                        <Link
+                          key={subItem.name}
+                          href={subItem.path}
+                          className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+                          <div className="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                            {subItem.name === "Job Seekers" ? (
+                              <FaUser className="text-primary text-lg" />
+                            ) : (
+                              <FaUsers className="text-primary text-2xl" />
+                            )}
+                          </div>
+                          <div>
+                            <span className="font-semibold text-gray-900">
+                              {subItem.name}
+                            </span>
+                            <p className="mt-1 text-gray-600">
+                              For {subItem.name} / Candidates
+                            </p>
+                          </div>
+                        </Link>
+                      ))}
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              ) : (
+                <Link href={item.path}>{item.name}</Link>
+              )}
             </motion.div>
           ))}
           <motion.button
@@ -80,29 +124,55 @@ const Navbar = () => {
         </div>
         {isOpen && (
           <div className="absolute top-16 left-0 w-full bg-[#081321] text-white flex flex-col items-center space-y-4 py-4 md:hidden">
-            {[
-              "",
-              "home",
-              "about",
-              "services",
-              "resources",
-              "contact",
-              "careers",
-            ].map((item) => (
+            {menuItems.map((item) => (
               <motion.div
-                key={item}
+                key={item.name}
                 className={`text-[#4DCCE6] transition-colors duration-200 ${
-                  isActive(item === "home" ? "/" : `/${item}`)
+                  isActive(item.path)
                     ? "text-[#4DCCE6] font-bold"
                     : "hover:text-[#4DCCE6]"
                 }`}
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}>
-                <Link
-                  href={item === "home" ? "/" : `/${item}`}
-                  onClick={closeMenu}>
-                  {item.charAt(0).toUpperCase() + item.slice(1)}
-                </Link>
+                {item.submenu ? (
+                  <Popover>
+                    <PopoverTrigger className="inline-flex items-center gap-x-1">
+                      <span>{item.name}</span>
+                      {/* <FaAngleDown className="text-primary-light" /> */}
+                    </PopoverTrigger>
+
+                    <PopoverContent className="w-screen max-w-sm flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 ring-1 shadow-lg ring-gray-900/5">
+                      <div className="flex flex-col p-4 bg-semantic-white">
+                        {item.submenu.map((subItem) => (
+                          <Link
+                            key={subItem.name}
+                            href={subItem.path}
+                            className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+                            <div className="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                              {subItem.name === "Job Seekers" ? (
+                                <FaUser className="text-primary text-lg" />
+                              ) : (
+                                <FaUsers className="text-primary text-2xl" />
+                              )}
+                            </div>
+                            <div>
+                              <span className="font-semibold text-gray-900">
+                                {subItem.name}
+                              </span>
+                              <p className="mt-1 text-gray-600">
+                                For {subItem.name} / Candidates
+                              </p>
+                            </div>
+                          </Link>
+                        ))}
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                ) : (
+                  <Link href={item.path} onClick={closeMenu}>
+                    {item.name}
+                  </Link>
+                )}
               </motion.div>
             ))}
             <motion.button
