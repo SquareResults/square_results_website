224f46105f9d1e94b4ebf11ad96c6d6ffc183cae
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2025-05-06 18:48:57 -0400

    updates footer with new design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,124 +1,127 @@
+import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
+  const theCompanyLinks = [
+    { name: "About Us", href: "/about" },
+    { name: "Careers", href: "/careers" },
+    // { name: "FAQs", href: "/faqs" },
+    { name: "Resources", href: "/resources" },
+  ];
+
+  const servicesLinks = [
+    { name: "The Radar List", href: "https://theradarlist.com/" },
+    { name: "SquareTop", href: "https://squaretop.group/" },
+    { name: "SquareCircle", href: "https://squarecircle.group/" },
+    // { name: "SWFT", href: "#" },
+  ];
+
+  const socialMediaLinks = [
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/people/Theradarlist/100092114480865/",
+      icon: <FaFacebook size={24} />,
+    },
+    {
+      name: "Twitter",
+      href: "https://twitter.com/the_radar_list",
+      icon: <FaTwitter size={24} />,
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/company/squareresults/about/",
+      icon: <FaLinkedin size={24} />,
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/theradarlist/",
+      icon: <FaInstagram size={24} />,
+    },
+  ];
+
   return (
-    <footer className="bg-[#081321] text-white py-10">
-      <div className="container mx-auto px-12">
-        <div className="flex flex-col md:flex-row justify-between items-start md:space-x-10">
-          <div className="mb-4 md:mb-0">
-            <img
-              src="/images/SQRlogo.jpg"
-              alt="Logo"
-              className="w-25 h-25 space-y-20"
-            />
-          </div>
-          <div className="flex flex-col md:flex-row items-start space-y-30 md:space-y-0 md:space-x-8">
-            <div className="flex flex-col">
-              <h2 className="text-sm font-semibold text-[#4DCCE6] mb-4">
-                Terms and Conditions
-              </h2>
-              <p className="text-xs text-white leading-relaxed mb-4">
-                Welcome to SquareResults. These terms and conditions outline the
-                rules and regulations for the use of our website. By accessing
-                this website, we assume you accept these terms and conditions.
-                Do not continue to use SquareResults if you do not agree to all
-                of the terms and conditions stated on this page.
-              </p>
-              <p className="text-xs text-white leading-relaxed">
-                The following terminology applies to these Terms and Conditions,
-                Privacy Statement and Disclaimer Notice and all Agreements:
-                "Client", "You" and "Your" refers to you, the person log on this
-                website and compliant to the Company’s terms and conditions.
-                "The Company", "Ourselves", "We", "Our" and "Us", refers to our
-                Company. "Party", "Parties", or "Us", refers to both the Client
-                and ourselves.
-              </p>
+    <footer className="bg-primary-dark text-white p-6">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-start">
+          <div className="w-3/4">
+            <div className="w-1/4">
+              <img src="/images/SQRlogo.jpg" alt="Logo" className="w-25 h-25" />
             </div>
-            <div className="flex flex-col">
-              <h2 className="text-sm font-semibold text-[#4DCCE6] mb-4">
-                Legal Policies
+            <div className="flex flex-row my-2 center gap-2 md:mb-0">
+              <h2 className="text-sm font-semibold text-primary-light mb-4">
+                Terms &#124;
               </h2>
-              <p className="text-xs text-white leading-relaxed mb-4">
-                At SquareResults, we are committed to protecting your privacy.
-                This legal policy explains how we collect, use, disclose, and
-                safeguard your information when you visit our website.
-              </p>
-              <p className="text-xs text-white leading-relaxed">
-                We may update this policy from time to time to reflect changes
-                in our practices or for other operational, legal, or regulatory
-                reasons. If we make material changes to this policy, we will
-                notify you by email or through a notice on our website.
-              </p>
-            </div>
-            <div className="flex flex-col">
-              <h2 className="text-sm font-semibold text-[#4DCCE6] mb-4">
+              <h2 className="text-sm font-semibold text-primary-light mb-4">
+                Legal Policies &#124;
+              </h2>
+              <h2 className="text-sm font-semibold text-primary-light mb-4">
                 Cookie Policy
               </h2>
-              <p className="text-xs text-white leading-relaxed mb-4">
-                Our website uses cookies to improve your experience while you
-                navigate through the website. Out of these cookies, the cookies
-                that are categorized as necessary are stored on your browser as
-                they are essential for the working of basic functionalities of
-                the website.
-              </p>
-              <p className="text-xs text-white leading-relaxed">
-                We also use third-party cookies that help us analyze and
-                understand how you use this website. These cookies will be
-                stored in your browser only with your consent. You also have the
-                option to opt-out of these cookies. But opting out of some of
-                these cookies may have an effect on your browsing experience.
-              </p>
             </div>
-            <div className="flex flex-col">
-              <h2 className="text-sm font-semibold text-[#4DCCE6] mb-4">
+            <div className="my-2 text-gray-300 mx-auto">
+              &copy; {new Date().getFullYear()} SquareResults. All rights
+              reserved.
+            </div>
+          </div>
+          <div className="w-full md:w-2/4 sm:w-full flex flex-row justify-between md:flex-row space-y-30 md:space-y-0">
+            {/* ===== The Company ===== */}
+            <div className="flex flex-col gap-2">
+              <h2 className="text-sm font-semibold text-primary-light">
+                The Company
+              </h2>
+              {theCompanyLinks.map((link) => (
+                <Link
+                  key={link.name}
+                  href={link.href}
+                  className="text-xs leading-relaxed hover:underline hover:text-primary-light">
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+
+            {/* ===== Services ===== */}
+            <div className="flex flex-col gap-2">
+              <h2 className="text-sm font-semibold text-primary-light">
+                Services
+              </h2>
+              {servicesLinks.map((link) => (
+                <Link
+                  key={link.name}
+                  href={link.href}
+                  target="_blank"
+                  className="text-xs leading-relaxed hover:underline hover:text-primary-light">
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-sm font-semibold text-primary-light">
                 Our Address
               </h2>
-              <p className="text-xs text-white leading-relaxed mb-4">
-                410 N Scottsdale Rd, Tempe, AZ 85288
-              </p>
-              {/* TODO: remove if not needed
-              <p className="text-xs text-white leading-relaxed mb-4 text-justify">
-                Suite 100, City,
-              </p>
-              <p className="text-xs text-white leading-relaxed mb-4 text-justify">
-                State, 12345
-              </p> */}
-              <div className="flex justify-center space-x-4 mb-4 text-justify">
-                <a
-                  href="https://www.facebook.com/people/Theradarlist/100092114480865/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white hover:text-[#4DCCE6] transition-colors duration-200">
-                  <FaFacebook size={24} />
-                </a>
-                <a
-                  href="https://twitter.com/the_radar_list"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white hover:text-[#4DCCE6] transition-colors duration-200">
-                  <FaTwitter size={24} />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/squareresults/about/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white hover:text-[#4DCCE6] transition-colors duration-200">
-                  <FaLinkedin size={24} />
-                </a>
-                <a
-                  href="https://www.instagram.com/theradarlist/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white hover:text-[#4DCCE6] transition-colors duration-200">
-                  <FaInstagram size={24} />
-                </a>
+              <Link
+                href="https://maps.app.goo.gl/qkoPyD1LY8Gu9S9K7"
+                target="_blank"
+                className="hover:underline hover:text-primary-light">
+                <p className="text-xs leading-relaxed">410 N Scottsdale Rd,</p>
+                <p className="text-xs leading-relaxed">Tempe, AZ 85288</p>
+              </Link>
+
+              {/* ===== Social Media ===== */}
+              <div className="flex justify-center space-x-4 text-justify">
+                {socialMediaLinks.map((link) => (
+                  <Link
+                    key={link.name}
+                    href={link.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-primary-light transition-colors duration-200">
+                    {link.icon}
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
-        </div>
-        <div className="mt-8 text-center text-gray-300 mx-auto">
-          &copy; {new Date().getFullYear()} SquareResults. All rights reserved.
         </div>
       </div>
     </footer>
