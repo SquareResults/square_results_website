3cd9084294e2f04a452d75615f8dc5e87a30ac75
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2025-07-11 18:15:32 -0400

    chore: updating contact us form api

--- components/DemoForm/formSchema.ts
+++ components/DemoForm/formSchema.ts
@@ -4,12 +4,12 @@ export const formSchema = z.object({
   firstName: z.string().min(2, "First name must be at least 2 characters"),
   lastName: z.string().min(2, "Last name must be at least 2 characters"),
   email: z.string().email("Invalid email address"),
-  phone: z
+  phoneNumber: z
     .string()
     .min(10, "Phone number must be at least 10 digits")
     .optional()
     .or(z.literal("")),
-  jobRole: z.string().min(2, "Job role must be at least 2 characters"),
+  inquiryType: z.string().min(2, "At least a selection is required"),
   message: z.string().min(10, "Message must be at least 10 characters"),
 });
 
